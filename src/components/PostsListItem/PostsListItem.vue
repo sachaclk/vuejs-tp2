29ac11b19d0cca8aae2e6aee874816bc7adfdd6d
Sacha Malka <sacha.malka@etu-webschoolfactory.fr>
2017-03-14 11:15:28 +0100

    improve card style
    Signed-off-by: Sacha Malka <sacha.malka@etu-webschoolfactory.fr>

--- src/components/PostsListItem/PostsListItem.vue
+++ src/components/PostsListItem/PostsListItem.vue
@@ -4,14 +4,14 @@
 <!-- HTML -->
 <template>
 	<li class="posts-list-item" >
-	<router-link :to="{ name: 'post_component', params: { postId: proppost.id} }" class="posts-list-item__a">
-		<!-- <a class="posts-list-item__a" :href="proppost.redirect_url" target="_blank"> -->
-			<img :src="proppost.thumbnail.image_url" alt="">
-		<!-- </a> -->
-		<h2 class="posts-list-item__h2">{{proppost.name}}</h2>
+		<router-link :to="{ name: 'post_component', params: { postId: proppost.id} }">
+			<div  class="posts-list-item__a">
+				<img :src="proppost.thumbnail.image_url" alt="post image">
+			</div>
+			<h2 class="posts-list-item__h2">{{proppost.name}}</h2>
+		</router-link>
 		<p class="posts-list-item__p">{{proppost.votes_count}} upvotes</p>
 		<h4 class="posts-list-item__h4">{{proppost.tagline}}</h4>
-	</router-link>
 	</li>
 </template>
 
