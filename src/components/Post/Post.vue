bef1cc0456baee10888e45c101c94a1f211b4cf6
DESKTOP-ENR08NF\Guillaume Lechaire <guillaume.lechaire@etu-webschoolfactory.fr>
2017-03-16 17:39:31 +0100

    Début de post

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -3,15 +3,50 @@
 
 <!-- HTML -->
 <template>
-    <div>
+    <div v-if="post">
         <span>Post :</span>
-        <span>{{ this.$route.params.postId}}</span>
+        <span>{{ post.name}}</span>
+        <img :src="post.thumbnail.image_url">
+        <!--<span>{{ post.}}</span>-->
+        <span>Nombre de commentaires : {{ post.comments_count}}</span>
     </div>
 </template>
 
 <!-- JS -->
 <script>
+
+    import axios from 'axios'
+    import _ from 'lodash'
+
     export default {
+
+
+
+        data() {
+            return {
+                post: null
+            }
+        },
+
+        created() {
+            this.getPost(this.$route.params.postId)
+        },
+        methods: {
+
+            getPost(postId) {
+                // Utilisez axios pour récupérer les posts de l'API ProductHunt
+                // Variable à modifier : this.posts
+                axios.get('/posts/' + postId)
+                    .then((response) => {
+                        console.log(response.data.post)
+                        this.post = response.data.post
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    })
+            }
+
+        }
 
     }
 </script>
